1c0e9e3b3203f296e68e37570ee0ccfed7e50c84
Thiago <thiago.santos@wansoft.com.br>
2025-03-04 20:23:37 -0300

    configures employee table

---
+++ src/components/EmployeeTable/index.jsx
@@ -0,0 +1,46 @@
+import './style.css'
+
+const EmployeeTable = ({ employees, loading }) => {
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
+
+    if (employees.length === 0) {
+        return <div>Nenhum colaborador encontrado</div>;
+    }
+
+    return (
+        <div className="table-container">
+            <table className="employee-table">
+                <thead>
+                    <tr>
+                        <th>FOTO</th>
+                        <th>NOME</th>
+                        <th>CARGO</th>
+                        <th>DATA DE ADMISSÃO</th>
+                        <th>TELEFONE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map((employee) => (
+                        <tr key={employee.id}>
+                            <td>
+                                <img
+                                    src={employee.image}
+                                    alt="Foto do colaborador"
+                                    className="employee-image"
+                                />
+                            </td>
+                            <td>{employee.name}</td>
+                            <td>{employee.job}</td>
+                            <td>{employee.admission_date}</td>
+                            <td>{employee.phone}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default EmployeeTable
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,30 @@
 import './style.css'
 import { fetchEmployees } from '../../services/api'
 import { useState, useEffect } from 'react';
+import EmployeeTable from '../../components/EmployeeTable';
 
 function Home() {
 
   const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getEmployees = async () => {
+      setLoading(true);
       const data = await fetchEmployees();
       setEmployees(data);
+      setLoading(false);
     };
 
     getEmployees();
   }, []);
 
   return (
+
     <div>
-      <h1>Funcionários</h1>
-      <ul>
-        {employees.map(employee => (
-          <li key={employee.id}>{employee.name}</li>
-        ))}
-      </ul>
+      <EmployeeTable employees={employees} loading={loading} />
     </div>
+
   )
 }
 
